5a7c310dedee290c185d2d4b9f493bde04ae4e27
Angelo Alfano <alfano.angelo97@gmail.com>
2018-05-29 17:23:58 +0200

    First
    First commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public PlayerController thePlayer;
+
+	private Vector3 lastPlayerPosition;
+	private float distanceToMove;
+
+	// Use this for initialization
+	void Start () {
+		thePlayer = FindObjectOfType<PlayerController>();
+		lastPlayerPosition = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		distanceToMove = thePlayer.transform.position.x - lastPlayerPosition.x;
+		transform.position = new Vector3(transform.position.x + distanceToMove, transform.position.y, transform.position.z);
+
+		lastPlayerPosition = thePlayer.transform.position;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpForce;
+	public bool grounded;
+	public LayerMask whatIsGround; //selezione del layer che ho inserito su Unity
+
+	private Rigidbody2D myRigidbody;
+	private Collider2D myCollider;
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Se il collider del player sta toccando il collider del Ground
+		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround); 
+
+		myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y);
+
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+			
+			if(grounded) //Lo faccio saltare solo quando tocca il terreno
+				myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+		
+		}
+
+		//Do i valori all'Animator in modo che possa gestire le animazioni da come settato
+		// nell'editor
+		myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
+		myAnimator.SetBool("Grounded", grounded);
+	}
+}
