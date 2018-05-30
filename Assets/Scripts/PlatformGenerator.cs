dbf8a159f62e9c98d5fb026c78955e0d719bceda
Angelo Alfano <alfano.angelo97@gmail.com>
2018-05-30 17:09:17 +0200

    Platform Random Heights

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,30 +5,38 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour {
 
 	public GameObject thePlatform;
-	//public GameObject[] thePlatforms;
-
 	
 	public Transform generationPoint;
 	
 	public float distanceBetween;
 	public float distanceBetweenMin;
 	public float distanceBetweenMax;
+	public float maxHeightChange;
 	
 	public ObjectPooler[] theObjectPools;
+
+	public Transform maxHeightPoint;
 
 	private float platformWidth;
 	private int platformSelector;
 	private float[] platformWidths;
 	
+	private float minHeight;
+	private float maxHeight;
+	private float heightChange;
+
+	
 
 	// Use this for initialization
 	void Start () {
-		//platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
 		platformWidths = new float[theObjectPools.Length];
 
 		for(int i=0; i<theObjectPools.Length; i++){
 			platformWidths[i] = theObjectPools[i].pooledObject.GetComponent<BoxCollider2D>().size.x;
 		}
+
+		minHeight = transform.position.y;
+		maxHeight = maxHeightPoint.position.y;
 	}
 	
 	// Update is called once per frame
@@ -40,20 +48,24 @@ public class PlatformGenerator : MonoBehaviour {
 			
 			platformSelector = Random.Range(0,theObjectPools.Length);
 
-			transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, transform.position.y, transform.position.z);
+			heightChange = transform.position.y + Random.Range(maxHeightChange,-maxHeightChange);
+
+			if(heightChange > maxHeight){
+				heightChange = maxHeight;
+			}
+			else if (heightChange < minHeight){
+				heightChange = minHeight;
+			}
 			
-			
-			//PER INSTANZIARE COSE RANDOM
-			//Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
-			
-			
+			transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2) + distanceBetween, heightChange, transform.position.z);
+		
 			//Crea una nuova piattaforma
 			GameObject newPlatform = theObjectPools[platformSelector].GetPooledObject();
 			newPlatform.transform.position = transform.position;
 			newPlatform.transform.rotation = transform.rotation;
 			newPlatform.SetActive(true);
 
-			transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.y, transform.position.z);
+			transform.position = new Vector3(transform.position.x + (platformWidths[platformSelector] / 2), transform.position.z);
 		}
 			
 	}
