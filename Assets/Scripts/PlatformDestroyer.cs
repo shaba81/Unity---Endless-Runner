9eef4f082e17e33ddcea87eeda7a174e4b971cab
Angelo Alfano <alfano.angelo97@gmail.com>
2018-05-30 15:46:19 +0200

    Object Pooling and Platform Randomizer

---
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	public GameObject platformDestructionPoint;
+
+	// Use this for initialization
+	void Start () {
+		platformDestructionPoint = GameObject.Find("PlatformDestructionPoint");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(transform.position.x < platformDestructionPoint.transform.position.x){
+			//Destroy (gameObject);
+
+			//Usiamo object pooling, le piattaforme da distruggere le rendiamo non attive
+			//In modo da poterle riutilizzare
+			
+			gameObject.SetActive(false);
+		}
+	}
+}
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,23 +5,53 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour {
 
 	public GameObject thePlatform;
+	public GameObject[] thePlatforms;
+	
 	public Transform generationPoint;
+	
 	public float distanceBetween;
+	public float distanceBetweenMin;
+	public float distanceBetweenMax;
+	
+	public ObjectPooler theObjectPool;
 
 	private float platformWidth;
+	private int platformSelector;
+	private float[] platformWidths;
+	
 
 	// Use this for initialization
 	void Start () {
-		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+		//platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+		platformWidths = new float[thePlatforms.Length];
+
+		for(int i=0; i<thePlatforms.Length ; i++){
+			platformWidths[i] = thePlatforms[i].GetComponent<BoxCollider2D>().size.x;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 		if(transform.position.x < generationPoint.position.x){
-			transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween, transform.position.y, transform.position.z);
+			
+			distanceBetween = Random.Range(distanceBetweenMin,distanceBetweenMax);
+			
+			platformSelector = Random.Range(0,thePlatforms.Length);
+
+			transform.position = new Vector3(transform.position.x + platformWidths[platformSelector] + distanceBetween, transform.position.y, transform.position.z);
+			
+			
+			//PER INSTANZIARE COSE RANDOM
+			Instantiate (thePlatforms[platformSelector], transform.position, transform.rotation);
+			
+			
 			//Crea una nuova piattaforma
-			Instantiate (thePlatform, transform.position, transform.rotation);
+			/*GameObject newPlatform = theObjectPool.GetPooledObject();
+			newPlatform.transform.position = transform.position;
+			newPlatform.transform.rotation = transform.rotation;
+			newPlatform.SetActive(true);*/
 		}
+			
 	}
 }
