a38d515a4b85d1fd1c386a6e5a54f28b98686d17
Angelo Alfano <alfano.angelo97@gmail.com>
2018-05-31 20:09:11 +0200

    Speed Increase and Death

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public Transform platformGenerator;
+	public PlayerController thePlayer;
+
+	private Vector3 platformStartPoint;
+	private Vector3 playerStartPoint;
+	private PlatformDestroyer[] platformList;
+
+	// Use this for initialization
+	void Start () {
+		platformStartPoint = platformGenerator.position;
+		playerStartPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RestartGame(){
+		StartCoroutine ("RestartGameCo");
+	}
+
+	public IEnumerator RestartGameCo(){
+		thePlayer.gameObject.SetActive(false);
+		yield return new WaitForSeconds(0.5f);
+		platformList = FindObjectsOfType<PlatformDestroyer>();
+		for(int i=0; i<platformList.Length; i++){
+			platformList[i].gameObject.SetActive(false);
+		}
+		thePlayer.transform.position = playerStartPoint;
+		platformGenerator.position = platformStartPoint;
+		thePlayer.gameObject.SetActive(true);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,60 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed;
+	public float speedMultiplier;
+	public float speedIncreaseMilestone;
 	public float jumpForce;
 	public float jumpTime;
 	public bool grounded;
 	public LayerMask whatIsGround; //selezione del layer che ho inserito su Unity
+	public Transform groundCheck;
+	public float groundCheckRadius;
+	public GameManager theGameManager;
 
 	private Rigidbody2D myRigidbody;
-	private Collider2D myCollider;
+	//private Collider2D myCollider;
 	private Animator myAnimator;
 	private float jumpTimeCounter;
+	private float speedMilestoneCount;
+	private float moveSpeedStore;
+	private float speedMileStoneCountStore;
+	private float speedIncreaseMilestoneStore;
 
 	// Use this for initialization
 	void Start () {
 		myRigidbody = GetComponent<Rigidbody2D>();
-		myCollider = GetComponent<Collider2D>();
+		//myCollider = GetComponent<Collider2D>();
 		myAnimator = GetComponent<Animator>();
 		jumpTimeCounter = jumpTime;
+		speedMilestoneCount = speedIncreaseMilestone;
+		moveSpeedStore = moveSpeed;
+		speedMileStoneCountStore = speedMilestoneCount;
+		speedIncreaseMilestoneStore = speedIncreaseMilestone;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		//Se il collider del player sta toccando il collider del Ground
-		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround); 
+		//grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround); 
+
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+		//Il player va sempre più veloce quando percorre una certa distanza
+		if(transform.position.x > speedMilestoneCount){
+			speedMilestoneCount += speedIncreaseMilestone;
+			speedIncreaseMilestone = speedIncreaseMilestone * speedMultiplier;
+			moveSpeed = moveSpeed * speedMultiplier;
+		}
 
 		myRigidbody.velocity = new Vector2(moveSpeed,myRigidbody.velocity.y);
 
-		/*if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+		if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)){
 			
 			if(grounded) //Lo faccio saltare solo quando tocca il terreno
 				myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
 		
-		}*/
+		}
 
 		if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)){
 
@@ -60,5 +82,14 @@ public class PlayerController : MonoBehaviour {
 		// nell'editor
 		myAnimator.SetFloat("Speed", myRigidbody.velocity.x);
 		myAnimator.SetBool("Grounded", grounded);
+	}
+
+	void OnCollisionEnter2D (Collision2D other){
+		if(other.gameObject.tag == "killbox"){
+			theGameManager.RestartGame();
+			moveSpeed = moveSpeedStore;
+			speedMilestoneCount = speedMileStoneCountStore;
+			speedIncreaseMilestone = speedIncreaseMilestoneStore;
+		}
 	}
 }
