2e1b8ea92b22dff1a64b980a5a4970f128a64dc5
shaba81 <alfano.angelo97@gmail.com>
2018-06-07 15:12:09 +0200

    Main Menu added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour {
+
+	public string playGameLevel;
+
+	public void PlayGame(){
+		Application.LoadLevel(playGameLevel);
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
